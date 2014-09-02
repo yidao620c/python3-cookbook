8e07134c2540ac85479556de869842de8214e1ae
XiongNeng <yidao620@gmail.com>
2014-09-02 18:03:04 +0800

    1.15小节开始

---
+++ cookbook/c01/p15_group.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+Topic: 分组迭代
+Desc : 
+"""
+
+
+def group_iter():
+    rows = [
+        {'address': '5412 N CLARK', 'date': '07/01/2012'},
+        {'address': '5148 N CLARK', 'date': '07/04/2012'},
+        {'address': '5800 E 58TH', 'date': '07/02/2012'},
+        {'address': '2122 N CLARK', 'date': '07/03/2012'},
+        {'address': '5645 N RAVENSWOOD', 'date': '07/02/2012'},
+        {'address': '1060 W ADDISON', 'date': '07/02/2012'},
+        {'address': '4801 N BROADWAY', 'date': '07/01/2012'},
+        {'address': '1039 W GRANVILLE', 'date': '07/04/2012'},
+    ]
+
+
+
+if __name__ == '__main__':
+    group_iter()

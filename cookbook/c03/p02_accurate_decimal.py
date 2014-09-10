544fb1a238d1f5b281a99c94a6ccb7b30efda1de
XiongNeng <yidao620@gmail.com>
2014-09-10 23:19:41 +0800

    3.2小节完成

--- cookbook/c03/p02_accurate_decimal.py
+++ cookbook/c03/p02_accurate_decimal.py
@@ -5,7 +5,8 @@ Topic: 精确的浮点数运算
 Desc : 
 """
 from decimal import Decimal
-
+from decimal import localcontext
+import math
 
 
 def acc_deciamal():
@@ -20,6 +21,16 @@ def acc_deciamal():
     print(a + b)
     print((a + b) == Decimal('6.3'))
 
+    a = Decimal('1.3')
+    b = Decimal('1.7')
+    print(a / b)
+    with localcontext() as ctx:
+        ctx.prec = 3
+        print(a / b)
+
+    nums = [1.23e+18, 1, -1.23e+18]
+    print(sum(nums))
+    print(math.fsum(nums))
 
 
 if __name__ == '__main__':

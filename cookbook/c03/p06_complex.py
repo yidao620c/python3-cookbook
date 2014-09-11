f4d2e4575380abe0456dfb35b2d67eca380f4eb3
XiongNeng <yidao620@gmail.com>
2014-09-11 12:04:06 +0800

    3.6小节start

---
+++ cookbook/c03/p06_complex.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+Topic: sample
+Desc : 
+"""
+
+
+def complex_math():
+    a = complex(2, 4)
+    b = 3 - 5j
+
+
+
+if __name__ == '__main__':
+    complex_math()

477d377063c2b10b958efab42c2e487f4dd17b01
XiongNeng <yidao620@gmail.com>
2014-10-17 15:17:53 +0800

    正则替换高级主题

--- basic/regex/re_sub.py
+++ basic/regex/re_sub.py
@@ -12,6 +12,7 @@ class Nth(object):
     函数接受匹配到的那个 match object 作为参数，返回用来替换的字符串。
     利用这个特性就可以只在第 N 次匹配的时候返回要替换成的字符串，其他时候原样返回不做替换即可。
     """
+
     def __init__(self, nth, replacement):
         self.nth = nth
         self.replacement = replacement
@@ -52,15 +53,17 @@ def re_sub():
     a = pat.sub(Nth(3, 'bad'), 'This is a good story, good is good. Oh, good')
     print(a)
     # 传入一个lambda函数，在匹配处两边加双引号
-    a = pat.sub(lambda mo: '"' + mo.group(0) + '"', 'This is a good story, very good.')
+    a = pat.sub(lambda m: '"' + m.group(0) + '"', 'This is a good story, very good.')
     print(a)
 
     # 前后匹配，特殊构造，不作为分组
-    # (?=...)之后的字符串需要匹配表达式才能成功匹配
-    # (?<=...)之前的字符串需要匹配表达式才能成功匹配
-    pat = re.compile(r'(?<=a )good(?= story)')
-    a = pat.sub('bad', 'This is a good story, very good.')
-    print(a)
+    # 前向定界（只能写固定宽度的正则式）：(?<=...)之前的字符串需要匹配表达式才能成功匹配
+    # 后向定界(可以写任意正则式)：(?=...)之后的字符串需要匹配表达式才能成功匹配
+    pat = re.compile(r'(?<=(a){1} )good(?= (story){1,2})')
+    print(pat.sub('bad', 'This is a good story, very good.'))
+    # 所以如果想定位前面字符为可变长字符串时，需要使用到组
+    pat = re.compile(r'(a{1,2} )(good)(?= (story){1,2})')
+    print(pat.sub(lambda m: m.group(1) + 'bad', 'This is a good story, very good.'))
 
 
 if __name__ == '__main__':

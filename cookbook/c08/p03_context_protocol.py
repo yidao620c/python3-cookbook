128cecf074c25ca4dadd364acd702bcb7e517c71
XiongNeng <yidao620@gmail.com>
2015-01-27 15:44:12 +0800

    8.3小节完成~

--- cookbook/c08/p03_context_protocol.py
+++ cookbook/c08/p03_context_protocol.py
@@ -40,3 +40,29 @@ with conn as s:
     print(resp)
     # conn.__exit__() executes: connection closed
 
+
+class LazyConnection2:
+    def __init__(self, address, family=AF_INET, type=SOCK_STREAM):
+        self.address = address
+        self.family = family
+        self.type = type
+        self.connections = []
+
+    def __enter__(self):
+        sock = socket(self.family, self.type)
+        sock.connect(self.address)
+        self.connections.append(sock)
+        return sock
+
+    def __exit__(self, exc_ty, exc_val, tb):
+        self.connections.pop().close()
+
+# Example use
+from functools import partial
+
+conn = LazyConnection2(('www.python.org', 80))
+with conn as s1:
+    pass
+    with conn as s2:
+        pass
+    # s1 and s2 are independent sockets

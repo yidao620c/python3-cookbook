afd095e8d4a0a86f4a82ab24c5bb8050fbaa38ff
XiongNeng <yidao620@gmail.com>
2015-03-11 16:02:28 +0800

    数据库schema导出为excel格式演示

--- basic/maintest.py
+++ basic/maintest.py
@@ -9,7 +9,5 @@ import os
 
 if __name__ == '__main__':
     print('----start----')
-    print('abcd'[:-1])
-    print(type('abcd'[:-1]))
-    print(os.path.abspath(os.path.join(r'd:\tmp\work', '..', 'aaa.zip')))
+    print('aaa;'[:-1])
 
--- basic/myutils/excel/generate_schema.py
+++ basic/myutils/excel/generate_schema.py
@@ -11,7 +11,8 @@ from openpyxl.cell import get_column_letter
 from openpyxl.drawing import Image
 from openpyxl.writer.dump_worksheet import WriteOnlyCell
 from openpyxl.comments import Comment
-from openpyxl.styles import Style, PatternFill, Border, Side, Alignment, Protection, Font
+from openpyxl.styles import Style, PatternFill, Border, Side, Alignment, Protection, Font, Color
+from openpyxl.styles import colors, borders, fills
 
 
 def load_xlsx():
@@ -84,6 +85,8 @@ def load_schema(filename):
         for line in sqlfile:
             if line.startswith('--'):
                 each_table.insert(0, line.split('--')[1].strip())
+            elif 'DROP TABLE' in line:
+                each_table.insert(1, line.strip().split()[-1][:-1])
             elif ' COMMENT ' in line and 'ENGINE=' not in line:
                 col_arr = line.split()
                 col_name = col_arr[0]
@@ -102,27 +105,103 @@ def load_schema(filename):
 
 
 def write_dest(xlsx_name, schema_name):
+    border = Border(
+        left=Side(border_style=borders.BORDER_THIN, color='FF000000'),
+        right=Side(border_style=borders.BORDER_THIN, color='FF000000'),
+        top=Side(border_style=borders.BORDER_THIN, color='FF000000'),
+        bottom=Side(border_style=borders.BORDER_THIN, color='FF000000')
+    )
+    alignment = Alignment(horizontal='justify', vertical='bottom',
+                          text_rotation=0, wrap_text=False,
+                          shrink_to_fit=True, indent=0)
+    fill = PatternFill(fill_type=None, start_color='FFFFFFFF')
+    # 基本的样式
+    basic_style = Style(font=Font(name='Microsoft YaHei')
+                        , border=border, alignment=alignment
+                        , fill=fill)
+    title_style = basic_style.copy(
+        font=Font(name='Microsoft YaHei', b=True, size=20, color='00215757'),
+        alignment=Alignment(horizontal='center', vertical='bottom',
+                            text_rotation=0, wrap_text=False,
+                            shrink_to_fit=True, indent=0),
+        fill=PatternFill(fill_type=fills.FILL_SOLID, start_color='00B2CBED'))
+    header_style = basic_style.copy(
+        font=Font(name='Microsoft YaHei', b=True, size=15, color='00215757'),
+        fill=PatternFill(fill_type=fills.FILL_SOLID, start_color='00BAA87F'))
+    common_style = basic_style.copy()
+    link_style = basic_style.copy(font=Font(
+        name='Microsoft YaHei', color=colors.BLUE, underline='single'))
     table_data = load_schema(schema_name)
     wb = Workbook()
     wb.active.title = "首页列表"
+
     for table in table_data:
         ws = wb.create_sheet(title=table[0])
         ws.merge_cells('E3:H3')  # 合并单元格
+        ws['E3'].style = title_style
+        ws['F2'].style = Style(border=Border(
+            bottom=Side(border_style=borders.BORDER_THIN, color='FF000000')))
+        ws['G2'].style = Style(border=Border(
+            bottom=Side(border_style=borders.BORDER_THIN, color='FF000000')))
+        ws['H2'].style = Style(border=Border(
+            bottom=Side(border_style=borders.BORDER_THIN, color='FF000000')))
+        ws['I3'].style = Style(border=Border(
+            left=Side(border_style=borders.BORDER_THIN, color='FF000000')))
         ws['E3'] = table[0]
-        ws['E5'] = '列名'
-        ws['F5'] = '类型'
-        ws['G5'] = '空值约束'
-        ws['H5'] = '备注'
-        for idx, each_column in enumerate(table[1:]):
-            ws['E{}'.format(idx + 6)] = each_column[0]
-            ws['F{}'.format(idx + 6)] = each_column[1]
-            ws['G{}'.format(idx + 6)] = each_column[2]
-            ws['H{}'.format(idx + 6)] = each_column[3].strip().split('\'')[1]
+        ws['E4'].style = header_style
+        ws['E4'] = '列名'
+        ws['F4'].style = header_style
+        ws['F4'] = '类型'
+        ws['G4'].style = header_style
+        ws['G4'] = '空值约束'
+        ws['H4'].style = header_style
+        ws['H4'] = '备注'
+        ws.column_dimensions['E'].width = 20
+        ws.column_dimensions['F'].width = 20
+        ws.column_dimensions['G'].width = 16
+        ws.column_dimensions['H'].width = 45
+        for idx, each_column in enumerate(table[2:]):
+            ws['E{}'.format(idx + 5)].style = common_style
+            ws['E{}'.format(idx + 5)] = each_column[0]
+            ws['F{}'.format(idx + 5)].style = common_style
+            ws['F{}'.format(idx + 5)] = each_column[1]
+            ws['G{}'.format(idx + 5)].style = common_style
+            ws['G{}'.format(idx + 5)] = each_column[2]
+            ws['H{}'.format(idx + 5)].style = common_style
+            ws['H{}'.format(idx + 5)] = each_column[3].strip().split('\'')[1]
+    ws = wb['首页列表']
+    ws.merge_cells('D3:F3')
+    ws['D3'].style = title_style
+    ws['E2'].style = Style(border=Border(
+        bottom=Side(border_style=borders.BORDER_THIN, color='FF000000')))
+    ws['F2'].style = Style(border=Border(
+        bottom=Side(border_style=borders.BORDER_THIN, color='FF000000')))
+    ws['G3'].style = Style(border=Border(
+        left=Side(border_style=borders.BORDER_THIN, color='FF000000')))
+    ws['D3'] = '贷快发数据库系统表'
+    ws['D4'].style = header_style
+    ws['D4'] = '编号'
+    ws['E4'].style = header_style
+    ws['E4'] = '表名'
+    ws['F4'].style = header_style
+    ws['F4'] = '详情链接'
+    ws.column_dimensions['D'].width = 15
+    ws.column_dimensions['E'].width = 25
+    ws.column_dimensions['F'].width = 35
+    for inx, val in enumerate(table_data):
+        ws['D{}'.format(inx + 5)].style = common_style
+        ws['D{}'.format(inx + 5)] = inx + 1
+        ws['E{}'.format(inx + 5)].style = common_style
+        ws['E{}'.format(inx + 5)] = val[1]
+        linkcell = ws['F{}'.format(inx + 5)]
+        linkcell.style = link_style
+        linkcell.value = val[0]
+        linkcell.hyperlink = '#{0}!{1}'.format(val[0], 'E3')
     wb.save(filename=xlsx_name)
 
 
 if __name__ == '__main__':
     # write_xlsx()
     # write_only()
-    write_dest('aaa.xlsx', 'schema.sql')
+    write_dest('贷快发数据库设计.xlsx', 'schema.sql')
     pass

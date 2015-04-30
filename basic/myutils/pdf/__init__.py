94629d8f3d311ce82d6790906a3a7ec6d49154ec
XiongNeng <yidao620@gmail.com>
2015-04-30 16:26:48 +0800

    pdf读取和美化输出

---
+++ basic/myutils/pdf/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+Topic: sample
+Desc :
+
+Python操作PDF：
+
+1. 读取内容使用slate 0.4.1（基于PDFMiner），只能用于Python2
+2. 各种PDF操作，包括读取内容、合并、分割、旋转、提取页面等等使用PyPDF2
+
+目前最好的做法是使用PDFMiner，安装之后：
+pdf2txt.py -o pc.txt /home/mango/work/perfect.pdf
+然后自己去分享pc.txt文件即可
+"""
+
---
+++ basic/myutils/pdf/perfect_lines.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+Topic: 处理pdf2txt.py -o pc.txt /home/mango/work/perfect.pdf生成的txt文件
+Desc : 最后的结果是我想要的，去除了页头和页脚的部分
+"""
+import re
+
+def beauty(txt_file):
+    with open(txt_file, mode='r+', encoding='utf-8') as f:
+        lines = f.readlines()
+        f.seek(0)
+        for line in lines:
+            if line.startswith('www.it-ebooks.info'):
+                f.seek(f.tell() - 1)
+                if f.readline().startswith('Chapter '):
+                    # 回退7位
+                    f.seek(f.tell() - 7)
+                else:
+                    f.seek(f.tell() - 5)
+            else:
+                f.write(line)
+        f.truncate()
+
+
+def beauty2(txt_file):
+    with open(txt_file, mode='r', encoding='utf-8') as f:
+        lines = f.readlines()
+    result_lines = []
+    for i, line in enumerate(lines):
+        if line.startswith('www.it-ebooks.info'):
+            if result_lines[len(result_lines) - 2].startswith('Chapter '):
+                # 删除7
+                for k in range(7):
+                    result_lines.pop()
+            elif result_lines[len(result_lines) - 4].startswith('| '):
+                # 删除5行
+                for k in range(5):
+                    result_lines.pop()
+            else:
+                check_str = result_lines[len(result_lines) - 2].strip()
+                if re.match('\d{3}', check_str):
+                    # 删除3行
+                    for k in range(3):
+                        result_lines.pop()
+        else:
+            result_lines.append(line)
+
+    # 结果写入
+    with open(txt_file, mode='w', encoding='utf-8') as f:
+        f.writelines(result_lines)
+
+
+if __name__ == '__main__':
+    beauty2('pc.txt')

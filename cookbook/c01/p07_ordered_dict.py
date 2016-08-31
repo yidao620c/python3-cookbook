edbc90c00a26752460243a032c8fae38e90e96b7
xiuyanduan <xiuyanduan@gmail.com>
2016-08-31 13:48:16 +0800

    Update p07_ordered_dict.py
    Modify wrong retract code

--- cookbook/c01/p07_ordered_dict.py
+++ cookbook/c01/p07_ordered_dict.py
@@ -7,13 +7,13 @@ Desc :
 
 from collections import OrderedDict
 
-def ordered_dict():
-    d = OrderedDict()
-    d['foo'] = 1
-    d['bar'] = 2
-    d['spam'] = 3
-    d['grok'] = 4
-    # Outputs "foo 1", "bar 2", "spam 3", "grok 4"
-    for key in d:
-        print(key, d[key])
+
+d = OrderedDict()
+d['foo'] = 1
+d['bar'] = 2
+d['spam'] = 3
+d['grok'] = 4
+# Outputs "foo 1", "bar 2", "spam 3", "grok 4"
+for key in d:
+    print(key, d[key])
 

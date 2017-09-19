eb4a1370df0a8a15b3e3c2bc485a5a9e587ff0a4
cclauss <cclauss@bluewin.ch>
2017-09-19 20:45:29 +0800

    print() function for Python 3

--- exts/zh.py
+++ exts/zh.py
@@ -6,7 +6,7 @@ class SearchChinese(SearchLanguage):
     lang = 'zh'
 
     def init(self, options):
-        print "reading Chiniese dictionary"
+        print("reading Chiniese dictionary")
         self.seg = SEG() 
 
     def split(self, input):
